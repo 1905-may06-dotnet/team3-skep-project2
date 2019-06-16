c90735881445ebc451eb4ca495ecfed55fada9fc
kevin cloinger <kevin.cloinger@gmail.com>
2019-06-16 17:37:06 -0500

    add test covage to docker

--- Test/LoginTest.cs
+++ Test/LoginTest.cs
@@ -18,13 +18,8 @@ namespace Test
             //to use Mock you need to set up method as a virtual to override the method.
             var mock = new Mock<Data.Repo>();
             mock.Setup(x => x.UsernameExist("test")).Returns(true);
-            
             LoginController loginController = new LoginController(mock.Object);
-            
-
             var response = loginController.LookUpUser("test");
-
-
             Assert.IsNotNull(response);
             Assert.IsInstanceOfType(response, typeof(AcceptedResult));           
         }
