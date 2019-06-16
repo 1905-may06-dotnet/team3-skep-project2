4b77f920b31da6b94af787e920fe94b141b1c8f7
starkeycs <starkeycs@outlook.com>
2019-06-16 15:31:39 -0500

    tested controller on login passed on lookup

--- Test/LoginTest.cs
+++ Test/LoginTest.cs
@@ -1,4 +1,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using webapi.Controllers;
+using Domain;
+using Data;
+using System.Net.Http;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Test
 {
@@ -6,8 +13,25 @@ namespace Test
     public class LoginTest
     {
         [TestMethod]
-        public void TestMethod()
+        public void LookupTest()
         {
+            //to use Mock you need to set up method as a virtual to override the method.
+            var mock = new Mock<Data.Repo>();
+            mock.Setup(x => x.UsernameExist("test")).Returns(true);
+            
+            LoginController loginController = new LoginController(mock.Object);
+            
+
+            var response = loginController.LookUpUser("test");
+
+
+            Assert.IsNotNull(response);
+            Assert.IsInstanceOfType(response, typeof(AcceptedResult));
+
+
+
+            
+
         }
     }
 }
