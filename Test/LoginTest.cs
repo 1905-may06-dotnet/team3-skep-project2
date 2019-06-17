23c5e769ff7dd2608b4e49bdf8339ce4f3a56819
starkeycs <starkeycs@outlook.com>
2019-06-17 09:25:49 -0500

    Login method fully tested

--- Test/LoginTest.cs
+++ Test/LoginTest.cs
@@ -36,14 +36,14 @@ namespace Test
         }
 
         [TestMethod]
-        public void LoginTrue()
+        public void LoginNotFound()
         {
             //to use Mock you need to set up method as a virtual to override the method.
             var mock = new Mock<Data.Repo>();
             mock.Setup(x => x.PasswordMatched("test", "word")).Returns(true);
 
             LoginController loginController = new LoginController(mock.Object);
-            var testValue = new BGUser("test","word");
+            var testValue = new BGUser("Bob","fail");
 
             var response = loginController.UserLogin(testValue);
             //var contentResponse = response as Ok;
@@ -51,9 +51,48 @@ namespace Test
 
             Assert.IsNotNull(response);
             
-            //Assert.IsInstanceOfType(response, typeof(AcceptedResult));
+            Assert.IsInstanceOfType(response, typeof(NotFoundResult));
+        }
+        [TestMethod]
+        public void LoginUnauthorized()
+        {
+            //to use Mock you need to set up method as a virtual to override the method.
+            var mock = new Mock<Data.Repo>();
+            mock.Setup(x => x.UsernameExist("test")).Returns(true);
+            mock.Setup(x => x.PasswordMatched("test", "word")).Returns(false);
+
+            LoginController loginController = new LoginController(mock.Object);
+            var testValue = new BGUser("test", "word");
+
+            var response = loginController.UserLogin(testValue);
+            //var contentResponse = response as Ok;
+
+
+            Assert.IsNotNull(response);
+
+            Assert.IsInstanceOfType(response, typeof(UnauthorizedResult));
+        }
+        [TestMethod]
+        public void LoginAccepted()
+        {
+            //to use Mock you need to set up method as a virtual to override the method.
+            var mock = new Mock<Data.Repo>();
+            mock.Setup(x => x.UsernameExist("test")).Returns(true);
+            mock.Setup(x => x.PasswordMatched("test", "word")).Returns(true);
+
+            LoginController loginController = new LoginController(mock.Object);
+            var testValue = new BGUser("test", "word");
+
+            var response = loginController.UserLogin(testValue);
+            //var contentResponse = response as Ok;
+
+
+            Assert.IsNotNull(response);
+
+            Assert.IsInstanceOfType(response, typeof(AcceptedResult));
         }
 
-        
+
+
     }
 }
