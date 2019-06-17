4959ad1ec24c8f62f7ada073d0216f782743b784
starkeycs <starkeycs@outlook.com>
2019-06-17 09:11:30 -0500

    add lookupfail to LoginTest

--- Test/LoginTest.cs
+++ Test/LoginTest.cs
@@ -17,12 +17,24 @@ namespace Test
         {
             //to use Mock you need to set up method as a virtual to override the method.
             var mock = new Mock<Data.Repo>();
-            mock.Setup(x => x.UsernameExist("test")).Returns(true);           
+            mock.Setup(x => x.UsernameExist("test")).Returns(true);
             LoginController loginController = new LoginController(mock.Object);
             var response = loginController.LookUpUser("test");
             Assert.IsNotNull(response);
             Assert.IsInstanceOfType(response, typeof(AcceptedResult));
         }
+        [TestMethod]
+        public void LookupTestFail()
+        {
+            //to use Mock you need to set up method as a virtual to override the method.
+            var mock = new Mock<Data.Repo>();
+            mock.Setup(x => x.UsernameExist("test")).Returns(true);
+            LoginController loginController = new LoginController(mock.Object);
+            var response = loginController.LookUpUser("fail");
+            Assert.IsNotNull(response);
+            Assert.IsInstanceOfType(response, typeof(AcceptedResult));
+        }
+
         [TestMethod]
         public void LoginTrue()
         {
@@ -42,5 +54,6 @@ namespace Test
             //Assert.IsInstanceOfType(response, typeof(AcceptedResult));
         }
 
+        
     }
 }
