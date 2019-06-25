6228615924a9442f4f4c7211a192a26e88f292b8
SamEF <samelfanneri@gmail.com>
2019-06-25 09:38:56 -0500

    MeetingController updated change

--- webapi/Controllers/MeetingController.cs
+++ webapi/Controllers/MeetingController.cs
@@ -13,46 +13,52 @@ namespace webapi.Controllers
     public class MeetingController : ControllerBase
     {
         private readonly IRepo db;
+
         public MeetingController(IRepo db)
         {
             this.db = db;
         }
 
         // POST: api/Meeting
-        [HttpPost]
+        [HttpPost("CreateMeeting")]
         public ActionResult AddMeeting([FromBody] Domain.Meeting meeting)
         {
             try
             {
                 db.CreateMeeting(meeting);
-                return Created("uri", meeting.HostUid);
+                return Created("uri",meeting.HostUid);
             }
             catch
             {
                 return BadRequest();
             }
         }
+
         [HttpPost]
         public ActionResult SearchMeetingByLocation()
         {
             //working on this one -Patrick
             return NotFound();
         }
+
         [HttpPost]
         public ActionResult SearchMeetingByGame()
         {
             return NotFound();
         }
+
         // PUT: api/Meeting/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+
         }
     }
 }
